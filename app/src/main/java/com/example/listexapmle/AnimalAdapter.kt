7f8f0c3dd38e33d7441bc04d56e92fe04c21fd6b
Katri Vilonen <katri.vilonen@gmail.com>
2019-06-04 13:44:27 +0300

    initial commit

---
+++ app/src/main/java/com/example/listexapmle/AnimalAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.listexapmle
+
+import android.content.Context
+import android.view.LayoutInflater.from
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.animal_list_item.view.*
+
+class AnimalAdapter(val items: ArrayList<String>, val context: Context) :
+    RecyclerView.Adapter<ViewHolder>() {
+
+    //Täyttää näkymät
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(from(context).inflate(R.layout.animal_list_item, parent, false))
+    }
+
+    //Palauttaa eläinten määrän
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    //Bindaa jokaisen eläimen AllayListasta omaan näkymään
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtAnimal.text = items[position]
+    }
+}
+class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+    val txtAnimal = v.txt_animal
+}
---
+++ app/src/main/java/com/example/listexapmle/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.listexapmle
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val animals: ArrayList<String> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        addAnimals()
+
+        rv_animal_list.layoutManager = LinearLayoutManager(this)
+        //rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        rv_animal_list.adapter = AnimalAdapter(animals, this)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+    fun addAnimals(){
+        animals.add("cat")
+        animals.add("dog")
+        animals.add("parrot")
+        animals.add("owl")
+        animals.add("bird")
+        animals.add("raccoon")
+        animals.add("snake")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
